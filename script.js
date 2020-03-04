d9c416debb7cdb336d2229bb30d8fa1aa3d72b53
neilsduncan <43460281+neilsduncan@users.noreply.github.com>
2020-03-04 10:57:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("killSteve").addEventListener("click", function(event){
+//console.log("did a thingy");
+document.getElementById("killMe").innerHTML = "";
+document.getElementById("killSteve").innerHTML = "";
+
+
+});
